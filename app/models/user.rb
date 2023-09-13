7cd1f5ed89e7d423d1b2db966c3e5ce40150d141
Felipe Welter <felipenwelter@gmail.com>
2023-09-13 08:12:45 -0300

    build User model and add bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
 class User < ApplicationRecord
+    has_secure_password
+    # it adds a password and a password confirmation to the User
+    # the attributes will run into a bcrypt and hash it to be saved in the database
 end
