f37ba8b00a6e9daa17b9447e44b74b8819e4bea6
Felipe Welter <felipenwelter@gmail.com>
2023-09-20 14:34:43 -0300

    adding email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
     has_secure_password
     # it adds a password and a password confirmation to the User
     # the attributes will run into a bcrypt and hash it to be saved in the database
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    #validates if the email is present when inserting a new user
+    #we can also set the rule on the migration to validate on the database level
 end
+
