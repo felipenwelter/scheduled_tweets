c99560bd66ba2f64dcab1c3a6ccb046d3ed6682e
Felipe Welter <felipenwelter@gmail.com>
2023-09-22 07:37:34 -0300

    handling sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,24 @@ class RegistrationsController < ApplicationController
         @user = User.new
         # this instance variable (@user) instead of a regular variable (local, this method) so it is visible in the view
     end
+
+    def create
+        # Parameters: {"authenticity_token"=>"[FILTERED]", "user"=>{"email"=>"a@a.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+        @user = User.new(user_params)
+        # "user"=>{"email"=>"a@a.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+        # this will raise an error in case there is no :user. Otherwise we could just use params[:user]
+    end
+
 end
