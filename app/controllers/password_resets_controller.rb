8fa62b91459e759c5e18e40b8f26137b286bb5bd
Felipe Welter <felipenwelter@gmail.com>
2023-09-28 19:51:43 -0300

    adding forgot password links and mailer

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,21 @@
+class PasswordResetsController < ApplicationController
+
+    def new
+        # just render the form
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+        
+        if @user.present?
+            # send email
+            PasswordMailer.with(user: @user).reset_deliver_now
+            #reset_deliver_now send now, take a second or more and block the request on the browser
+            #sends in a backgroud job, doesn't block the request on the browser
+        end
+
+        redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
+
+    end
+
+end
