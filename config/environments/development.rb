e7c91ed7bf2133b0c5793edf9ef1f9e60d046a58
Felipe Welter <felipenwelter@gmail.com>
2023-12-13 10:29:55 -0300

    chore: destroy dependencies when deleting account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.active_job.queue_adapter = :sidekiq
+
+  config.log_level = :debug #by default it seems it's already in debug level in development environment  
 end
